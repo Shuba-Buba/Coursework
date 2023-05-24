47367ddf6c13fa5f51b6dabb37e30f5a1236964a
Bulat Minnakhmetov <bulatminnakh@gmail.com>
2023-05-25 00:47:57 +0300

    Partially implement BinanceExchange

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -1,14 +1,83 @@
 package exchange
 
-import "trading/common/types"
+import (
+	"trading/common/types"
+)
+
+type OrderType string
+type OrderSide string
+type TimeInForceType string
+type RequestOption func(*PlaceOrderParams)
+
+type PlaceOrderParams struct {
+	symbol          string
+	side            OrderSide
+	quantity        float64
+	price           float64
+	timeInForceType TimeInForceType
+	orderType       OrderType
+}
+
+const (
+	OrderTypeMarket OrderType = "MARKET"
+	OrderTypeLimit  OrderType = "LIMIT"
+
+	TimeInForceTypeGTC TimeInForceType = "GTC" // Good Till Cancel
+	TimeInForceTypeIOC TimeInForceType = "IOC" // Immediate or Cancel
+	TimeInForceTypeFOK TimeInForceType = "FOK" // Fill or Kill
+	TimeInForceTypeGTX TimeInForceType = "GTX" // Good Till Crossing (Post Only)
+
+	OrderSideBuy  OrderSide = "BUY"
+	OrderSideSell OrderSide = "SELL"
+)
+
+type PlaceOrderBuilder struct {
+	params   *PlaceOrderParams
+	exchange Exchange
+}
+
+func (t *PlaceOrderBuilder) OrderSide(side OrderSide) *PlaceOrderBuilder {
+	t.params.side = side
+	return t
+}
+
+func (t *PlaceOrderBuilder) Symbol(symbol string) *PlaceOrderBuilder {
+	t.params.symbol = symbol
+	return t
+}
+
+func (t *PlaceOrderBuilder) Quantity(quantity float64) *PlaceOrderBuilder {
+	t.params.quantity = quantity
+	return t
+}
+
+func (t *PlaceOrderBuilder) Price(price float64) *PlaceOrderBuilder {
+	t.params.price = price
+	return t
+}
+
+func (t *PlaceOrderBuilder) TimeInForce(timeInForceType TimeInForceType) *PlaceOrderBuilder {
+	t.params.timeInForceType = timeInForceType
+	return t
+}
+
+func (t *PlaceOrderBuilder) OrderType(orderType OrderType) *PlaceOrderBuilder {
+	t.params.orderType = orderType
+	return t
+}
+
+func (t *PlaceOrderBuilder) Do() (string, error) {
+	return t.exchange.PlaceOrder(*t.params)
+}
 
 type Exchange interface {
-	PlaceOrder()
-	CancelOrder()
-	CancelAllOrders()
-	GetBalance()
-	GetOrderInfo()
+	Init()
+	PlaceOrder(PlaceOrderParams) (string, error)
+	CancelOrder(symbol string, orderId string) error
+	CancelAllOrders(symbol string) error
+	GetBalance() error
+	GetOrderInfo() OrderInfo
 	GetOrderbook() Orderbook
-	GetNewTrades()
 	Update(event types.Event)
+	Close()
 }
--- go.mod
+++ go.mod
@@ -3,4 +3,13 @@ module trading
 go 1.19
 
 require github.com/gorilla/websocket v1.5.0
+
 require github.com/valyala/fastjson v1.6.4
+
+require (
+	github.com/adshao/go-binance/v2 v2.4.2 // indirect
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
